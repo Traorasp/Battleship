045ce93b97d5b694dbc4328bbeccc42ade4d9344
Traorasp <miguelsolanopaz@outlook.com>
2022-06-05 03:13:57 -0400

    Set up basic foundation for project.
    Set up npm, webppack server, eslint and babel for testing. Created
    basic empty files for Battleships logic and a stylesheet.

---
+++ dist/Player.bundle.js
@@ -0,0 +1,18 @@
+(self["webpackChunkbattleship"] = self["webpackChunkbattleship"] || []).push([["Player"],{
+
+/***/ "./src/Player.js":
+/*!***********************!*\
+  !*** ./src/Player.js ***!
+  \***********************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/Player.js"));
+/******/ }
+]);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        Battleship: './src/Battleship.js',
+        Gameboard: './src/Gameboard.js',
+        Player: './src/Player.js',
+        Ship: './src/Ship.js',
+      },
+    devtool: 'inline-source-map',
+    devServer:  {
+        static: '.dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Battleship',
+        }),
+      ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+      },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+        ],
+      },
+  };
