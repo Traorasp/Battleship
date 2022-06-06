f4f548c5038ecc61c02b1c5300be74461458544a
Traorasp <miguelsolanopaz@outlook.com>
2022-06-05 21:06:19 -0400

    Added hit function and changed createShip test

--- src/Ship.test.js
+++ src/Ship.test.js
@@ -1,7 +1,13 @@
 import createShip from './createShip';
 
 test('creates a ship 3 long', () => {
-  expect(createShip(3)).toEqual({
-    ship: [1, 1, 1],
-  });
+  expect(createShip(3).ship).toEqual([1, 1, 1]);
+});
+
+test('Hit 3rd part of a ship', () => {
+  const boat = createShip(3);
+  boat.hit(3);
+  expect(boat.ship).toEqual(
+    [1, 1, 0],
+  );
 });
--- src/createShip.js
+++ src/createShip.js
@@ -3,8 +3,14 @@ const createShip = (size) => {
   for (let i = 0; i < size; i += 1) {
     ship.push(1);
   }
+
+  const hit = (shot) => {
+    ship[shot - 1] = 0;
+  };
+
   return {
     ship,
+    hit,
   };
 };
 
