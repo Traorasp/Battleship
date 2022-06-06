0739aa488346db81fbfe8c97d1d29a37aacc5d3e
Traorasp <miguelsolanopaz@outlook.com>
2022-06-06 01:44:13 -0400

    Added turn mechanic to player

--- src/Player.js
+++ src/Player.js
@@ -1,12 +1,22 @@
 import Gameboard from './Gameboard';
 
 const Player = (enemyBoard) => {
+  let turn = false;
+
   const attack = (x, y) => {
-    enemyBoard.receiveAttack(x, y);
+    if (turn) {
+      enemyBoard.receiveAttack(x, y);
+      turn = false;
+    }
+  };
+
+  const isTurn = () => {
+    turn = true;
   };
 
   return {
     attack,
+    isTurn,
   };
 };
 
--- src/Player.test.js
+++ src/Player.test.js
@@ -4,6 +4,14 @@ import Gameboard from './Gameboard';
 test('Player can attack oponents board', () => {
   const enemyBoard = Gameboard();
   const player = Player(enemyBoard);
+  player.isTurn();
   player.attack(2, 2);
   expect(enemyBoard.grid[1][1]).toBe(-1);
+});
+
+test('Test that player can not attck if it is not his turn', () => {
+  const enemyBoard = Gameboard();
+  const player = Player(enemyBoard);
+  player.attack(2, 2);
+  expect(enemyBoard.grid[1][1]).toBe(0);
 });
