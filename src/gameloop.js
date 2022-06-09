b74d02664910a147dd6c676777574c3bb51c4a3f
Traorasp <miguelsolanopaz@outlook.com>
2022-06-09 02:59:13 -0400

    Created function for sinking of ships
    When a ship is fully sunk all blank cells are changed to gray
    and all parts of the ship and changed to sunk.

--- src/gameloop.js
+++ src/gameloop.js
@@ -6,7 +6,7 @@ const gameloop = (() => {
   const boardTwo = Gameboard();
 
   boardOne.placeShip([1, 1, 1], [1, 2, 3]);
-  boardOne.placeShip([1, 2, 3], [1, 1, 1]);
+  boardOne.placeShip([7, 8, 9], [1, 1, 1]);
   boardOne.placeShip([5, 5], [8, 9]);
   boardOne.placeShip([8], [3]);
   boardOne.placeShip([2, 3], [6, 6]);
@@ -21,6 +21,89 @@ const gameloop = (() => {
   const p2 = Player(boardTwo);
 
   p1.isTurn();
+
+  const checkSides = (x, y, playersBoard) => {
+    const board = playersBoard === 1 ? document.querySelector('.P1 .board .grid')
+      : document.querySelector('.P2 .board .grid');
+    const gridChild = ((x - 1) * 10) + (y - 1);
+
+    const currCell = board.children.item(gridChild);
+    const currX = currCell.classList[0].slice(1);
+    const currY = currCell.classList[1].slice(1);
+
+    const relatedCells = [
+      board.children.item(gridChild - 10),
+      board.children.item(gridChild - 11),
+      board.children.item(gridChild - 1),
+      board.children.item(gridChild + 9),
+      board.children.item(gridChild + 10),
+      board.children.item(gridChild + 11),
+      board.children.item(gridChild + 1),
+      board.children.item(gridChild - 9),
+    ];
+
+    relatedCells.forEach((cell) => {
+      if (cell !== null) {
+        const cellX = cell.classList[0].slice(1);
+        const cellY = cell.classList[1].slice(1);
+        if ((currX !== cellX || (currX === cellX && Math.abs(currY - cellY) < 9))
+        && (currY !== cellY || (currY === cellY && Math.abs(currX - cellX) < 9))
+        && (Math.abs(currX - cellX) < 2) && (Math.abs(currY - cellY) < 2)) {
+          switch (cell.classList[3]) {
+            case 'miss':
+              break;
+            case 'sunk':
+              break;
+            case 'sunk-part':
+              cell.classList.remove('sunk-part');
+              cell.classList.add('sunk');
+              checkSides(cell.classList[0].slice(1), cell.classList[1].slice(1), playersBoard);
+              break;
+            default:
+              cell.classList.add('miss');
+              break;
+          }
+        }
+      }
+    });
+  };
+
+  const updateBoard = (x, y, playersBoard) => {
+    const board = playersBoard === 1 ? document.querySelector('.P1 .board .grid')
+      : document.querySelector('.P2 .board .grid');
+    const cell = board.children.item(((x - 1) * 10) + (y - 1));
+    switch (boardOne.cellStatus(x, y)) {
+      case 0:
+        break;
+      case -1:
+        cell.classList.add('miss');
+        break;
+      case -2:
+        cell.classList.add('sunk-part');
+        break;
+      default:
+        cell.classList.add('sunk');
+        checkSides(x, y, playersBoard);
+        break;
+    }
+  };
+
+  const round = (cell) => {
+    const x = cell.target.classList[0].slice(1);
+    const y = cell.target.classList[1].slice(1);
+    const playersBoard = cell.target.classList[2].slice(1);
+    if (playersBoard === 'P1' && !p1.attack(x, y)) {
+      if (!boardTwo.allSunk()) {
+        updateBoard(x, y, 1);
+        p2.isTurn();
+      }
+      if (!boardOne.allSunk()) {
+        const aiShot = p2.aiMove();
+        updateBoard(aiShot[0], aiShot[1], 2);
+        p1.isTurn();
+      }
+    }
+  };
 
   for (let i = 1; i <= 2; i += 1) {
     const player = i === 1 ? 'P1' : 'P2';
@@ -70,57 +153,6 @@ const gameloop = (() => {
       }
     }
   }
-
-  const updateBoard = (x, y, playersBoard) => {
-    if (playersBoard === 1) {
-      const cell = document.querySelector('.P1 .board .grid').children.item(((x - 1) * 10) + (y - 1));
-      switch (boardOne.cellStatus(x, y)) {
-        case 0:
-          break;
-        case -1:
-          cell.classList.add('miss');
-          break;
-        case -2:
-          cell.classList.add('sunk-part');
-          break;
-        default:
-          cell.classList.add('sunk');
-          break;
-      }
-    } else {
-      const cell = document.querySelector('.P2 .board .grid').children.item(((x - 1) * 10) + (y - 1));
-      switch (boardTwo.cellStatus(x, y)) {
-        case 0:
-          break;
-        case -1:
-          cell.classList.add('miss');
-          break;
-        case -2:
-          cell.classList.add('sunk-part');
-          break;
-        default:
-          cell.classList.add('sunk');
-          break;
-      }
-    }
-  };
-
-  const round = (cell) => {
-    const x = cell.target.classList[0].slice(1);
-    const y = cell.target.classList[1].slice(1);
-    const playersBoard = cell.target.classList[2].slice(1);
-    if (playersBoard === 'P1' && !p1.attack(x, y)) {
-      if (!boardTwo.allSunk()) {
-        updateBoard(x, y, 1);
-        p2.isTurn();
-      }
-      if (!boardOne.allSunk()) {
-        const aiShot = p2.aiMove();
-        updateBoard(aiShot[0], aiShot[1], 2);
-        p1.isTurn();
-      }
-    }
-  };
 })();
 
 export default gameloop;
